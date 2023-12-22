db28439f2f9f254107e9c40a04ee2cfff3ca5f91
JYOTISHANKAR PATRA <patrajyotishankar@gmail.com>
2023-12-23 00:35:27 +0530

    Final game

--- script.js
+++ script.js
@@ -1,87 +1,99 @@
+let mainPanel = document.querySelector("#mainPanel");
+let timerBox = document.querySelector("#timer");
+let startGame = document.querySelector("#startGame");
+let hitBox = document.querySelector("#hitVal");
+let scoreBox = document.querySelector("#scoreVal");
 let popUp = document.querySelector("#popUp");
-let newGame = document.querySelector("#newGame");
-let score = 0;
-let timer = 4;
-// !making bubble
-let hitRn = 0;
-let highScore = 0;
-function makeBubble() {
-  var clutter = "";
-  for (let i = 1; i <= 112; i++) {
-    let rn = Math.floor(Math.random() * 10);
-    clutter += ` <div class="bubble">${rn}</div>`;
-  }
-  document.getElementById("mainPanel").innerHTML = clutter;
-}
-// ! Timer function
+let newGameEL = document.querySelector("#newGame");
+let popUpScore = document.querySelector("#popUpScore");
+let popUpBestScore = document.querySelector("#popUpBestScore");
+let welcome = document.querySelector("#welcome");
+let byme = document.querySelector("#Byme");
+let titleGame = document.querySelector("#titleGame");
 
-function runTimer() {
-  var timerInterval = setInterval(() => {
-    if (timer > 0) {
-      timer--;
-      document.querySelector("#timer").innerText = timer;
+// !This clutter helps us to create new bubbles inside the main pannel
+
+//? variables
+let timeVar = 60;
+let clutter = ``;
+let hitVal;
+let currentScore = 0;
+let bestScore = 0;
+let makeBubbles = () => {
+  for (let i = 0; i <= 200; i++) {
+    let ren = Math.floor(Math.random() * 10);
+    // console.log("done");
+    clutter += `<div class="bubble">${ren}</div>`;
+  }
+  clutter = mainPanel.innerHTML = clutter;
+};
+
+// ? Timer function that changes time per second
+let runTimer = () => {
+  let timeTravel = setInterval(() => {
+    if (timeVar > 0) {
+      timeVar--;
+      timerBox.innerHTML = timeVar;
     } else {
-      document.querySelector("#timer").innerText = 60;
-      checkHighScore(document.querySelector("#scoreVal"));
-      document.querySelector("#hitVal").innerText = 0;
-      document.querySelector("#mainPanel").innerHTML = "";
-      clearInterval(timerInterval);
-      popUp.style.top = "10px";
-      checkHighScore();
+      displayScore();
+      mainPanel.innerHTML = "";
+      clearInterval(timeTravel);
+      // mainPanel.removeEventListener("click", false);
+      hitBox.innerText = 0;
+      popUp.style.top = "0";
+      currentScore = 0;
+      scoreBox.innerText = "0";
     }
   }, 1000);
-}
-
-function HitBox() {
-  hitRn = Math.floor(Math.random() * 10);
-  document.querySelector("#hitVal").innerText = hitRn;
-}
-
-let currentScore = (increaseScore = () => {
-  score += 10;
-  document.querySelector("#scoreVal").innerText = score;
-  return score;
-});
-
-HitBox();
-runTimer();
-makeBubble();
-document.querySelector("#mainPanel").addEventListener("click", (details) => {
-  let clickedNum = Number(details.target.innerText);
-  if (clickedNum === hitRn) {
-    currentScore();
-
-    makeBubble();
-    HitBox();
-  }
-});
-
-function updateScore() {
-  score = 0;
-  document.querySelector("#scoreVal").innerText = score;
-}
-let curScore = currentScore();
-
-function checkHighScore() {
-  if (curScore > highScore) {
-    highScore = curScore;
-    displayScore();
-  } else {
-    curScore = 0;
-    displayScore();
-  }
-}
-
-newGame.addEventListener("click", () => {
-  HitBox();
-  makeBubble();
-  popUp.style.top = "-100%";
-  runTimer();
-  timer = 10;
-  updateScore();
-});
-
-let displayScore = () => {
-  document.querySelector("#popUpScore").innerText = curScore;
-  document.querySelector("#popUpBestScore").innerText = highScore;
 };
+let displayScore = () => {
+  popUpBestScore.innerText = bestScore;
+  popUpScore.innerText = currentScore;
+};
+
+// ?Function for Hit box functionality
+let hitFun = () => {
+  let ran = Math.floor(Math.random() * 10);
+  hitVal = ran;
+  hitBox.innerText = ran;
+};
+// !Function to achive the clicked element and update the hit val and refresh bubble
+mainPanel.addEventListener("click", (details) => {
+  let clickedNum = Number(details.target.innerText);
+  increaseScore(clickedNum);
+  hitFun();
+  clutter = "";
+  makeBubbles();
+});
+
+function increaseScore(clickedNum) {
+  if (clickedNum === hitVal) {
+    currentScore += 10;
+    scoreBox.innerText = currentScore;
+    if (currentScore > bestScore) {
+      bestScore = currentScore;
+    }
+  }
+}
+
+let firstStartGame = () => {
+  startGame.addEventListener("click", () => {
+    startGame.style.display = "none";
+    makeBubbles();
+    runTimer();
+  });
+};
+
+firstStartGame();
+// increaseScore();
+
+newGameEL.addEventListener("click", () => {
+  popUp.style.top = "-100%";
+  timeVar = 60;
+  makeBubbles();
+  runTimer();
+});
+
+setTimeout(() => {
+  welcome.style.top = "100%";
+}, 2000);
